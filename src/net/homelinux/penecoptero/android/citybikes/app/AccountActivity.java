1698c6e3324c483757065e456d3089930f620001
csu333 <csu333-github@yahoo.fr>
2011-11-01 10:53:18 +0100

    Improved behaviour when first setting up the account

--- src/net/homelinux/penecoptero/android/citybikes/app/AccountActivity.java
+++ src/net/homelinux/penecoptero/android/citybikes/app/AccountActivity.java
@@ -17,88 +17,102 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class AccountActivity extends Activity {
-	
+
 	private EditText editText;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.account);
 		editText = (EditText) findViewById(R.id.editText);
-		
+
 		SharedPreferences settings = getSharedPreferences(CityBikes.PREFERENCES_NAME,0);
-		
+
+		String subscriberPIN = settings.getString("subscriberPIN", null);
+
+		// If subscriber PIN not known, shows the configuration screen
+		if (subscriberPIN == null || subscriberPIN.equals("")){
+			startActivity(new Intent(this, AccountSettingsActivity.class));
+		} 
+
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		SharedPreferences settings = getSharedPreferences(CityBikes.PREFERENCES_NAME,0);
+
 		String city = settings.getString("city", null);
 		String subscriberNumber = settings.getString("subscriberNumber", null);
 		String subscriberName = settings.getString("subscriberName", null);
 		String subscriberPIN = settings.getString("subscriberPIN", null);
 		
-		if (subscriberPIN == null){
-			startActivity(new Intent(this, AccountSettingsActivity.class));
-		} else {
-			JSONObject accountInfo = 
-					new AccountHelper().getSubscriberAccountInfo(
-						city, subscriberNumber, subscriberName, subscriberPIN);
-			editText.setText(new AccountHelper().getSubscriberAccountInfo(
+		// If subscriber PIN not known, close activity
+		if (subscriberPIN == null || subscriberPIN.equals("")){
+			finish();
+		}
+		
+		JSONObject accountInfo = 
+			new AccountHelper().getSubscriberAccountInfo(
+					city, subscriberNumber, subscriberName, subscriberPIN);
+		editText.setText(new AccountHelper().getSubscriberAccountInfo(
 				city, subscriberNumber, subscriberName, subscriberPIN).toString());
-			
-			try {
-				// Account info
-				TextView firstname = (TextView) findViewById(R.id.firstname);
-				firstname.setText(accountInfo.getString("firstname"));
-				
-				TextView lastname = (TextView) findViewById(R.id.lastname);
-				lastname.setText(accountInfo.getString("lastname"));
-				
-				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z'");
-				Date expiryDate = sdf.parse(accountInfo.getString("endValidity"));
-				expiryDate.setTime(expiryDate.getTime() + 
-						TimeZone.getDefault().getOffset(expiryDate.getTime()));
-				long daysToGo = expiryDate.getTime() - System.currentTimeMillis();				
-				
-				TextView accountValidity = (TextView) findViewById(R.id.account_validity);
-				accountValidity.setText(String.format(getString(R.string.days_to_go), 
-						expiryDate.toLocaleString(),
-						(daysToGo / DateUtils.DAY_IN_MILLIS)));	
-				
-				TextView accountState = (TextView) findViewById(R.id.account_state);
-				accountState.setText(
-						accountInfo.getString("account") + " " +
-						accountInfo.getString("currency"));	
-				
 
-				// Trip info
-				TextView inTrip = (TextView) findViewById(R.id.in_trip);
-				inTrip.setText(accountInfo.getString("inTrip"));
-				
-				expiryDate = sdf.parse(accountInfo.getString("lastTripDate"));
-				expiryDate.setTime(expiryDate.getTime() + 
-						TimeZone.getDefault().getOffset(expiryDate.getTime()));
-				
-				TextView lastTripDate = (TextView) findViewById(R.id.last_trip_date);
-				lastTripDate.setText(expiryDate.toLocaleString() + " (" +
-						DateUtils.getRelativeTimeSpanString(
-						expiryDate.getTime(), 
-						System.currentTimeMillis(), 
-						DateUtils.MINUTE_IN_MILLIS) + ")");
-				
-				TextView lastTripDuration = (TextView) findViewById(R.id.last_trip_duration);
-				lastTripDuration.setText(accountInfo.getString("lastTripDuration") + " " +
-						getString(R.string.minutes));
-				
-				TextView lastTripAmount = (TextView) findViewById(R.id.last_trip_amount);
-				lastTripAmount.setText(
-						accountInfo.getString("lastTripAmount") + " " + 
-						accountInfo.getString("currency"));
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		try {
+			// Account info
+			TextView firstname = (TextView) findViewById(R.id.firstname);
+			firstname.setText(accountInfo.getString("firstname"));
+
+			TextView lastname = (TextView) findViewById(R.id.lastname);
+			lastname.setText(accountInfo.getString("lastname"));
+
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z'");
+			Date expiryDate = sdf.parse(accountInfo.getString("endValidity"));
+			expiryDate.setTime(expiryDate.getTime() + 
+					TimeZone.getDefault().getOffset(expiryDate.getTime()));
+			long daysToGo = expiryDate.getTime() - System.currentTimeMillis();				
+
+			TextView accountValidity = (TextView) findViewById(R.id.account_validity);
+			accountValidity.setText(String.format(getString(R.string.days_to_go), 
+					expiryDate.toLocaleString(),
+					(daysToGo / DateUtils.DAY_IN_MILLIS)));	
+
+			TextView accountState = (TextView) findViewById(R.id.account_state);
+			accountState.setText(
+					accountInfo.getString("account") + " " +
+					accountInfo.getString("currency"));	
+
+
+			// Trip info
+			TextView inTrip = (TextView) findViewById(R.id.in_trip);
+			inTrip.setText(accountInfo.getString("inTrip"));
+
+			expiryDate = sdf.parse(accountInfo.getString("lastTripDate"));
+			expiryDate.setTime(expiryDate.getTime() + 
+					TimeZone.getDefault().getOffset(expiryDate.getTime()));
+
+			TextView lastTripDate = (TextView) findViewById(R.id.last_trip_date);
+			lastTripDate.setText(expiryDate.toLocaleString() + " (" +
+					DateUtils.getRelativeTimeSpanString(
+							expiryDate.getTime(), 
+							System.currentTimeMillis(), 
+							DateUtils.MINUTE_IN_MILLIS) + ")");
+
+			TextView lastTripDuration = (TextView) findViewById(R.id.last_trip_duration);
+			lastTripDuration.setText(accountInfo.getString("lastTripDuration") + " " +
+					getString(R.string.minutes));
+
+			TextView lastTripAmount = (TextView) findViewById(R.id.last_trip_amount);
+			lastTripAmount.setText(
+					accountInfo.getString("lastTripAmount") + " " + 
+					accountInfo.getString("currency"));
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
-
 }
